56e38f7bc2d25e59388a9c0f8b6b563cd8004cc8
BumBamBi <dlrhkddn0325@gmail.com>
2022-01-10 00:21:23 +0900

    API 방법 예제

--- hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -1,7 +1,5 @@
 package hello.hellospring.controller;
 
-import com.fasterxml.jackson.annotation.JsonSetter;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
