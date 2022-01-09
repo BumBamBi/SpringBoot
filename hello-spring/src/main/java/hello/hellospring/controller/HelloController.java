95d8a9b38d675e74ce122913bd4368775a000c3b
BumBamBi <dlrhkddn0325@gmail.com>
2022-01-09 23:44:25 +0900

    정적컨텐츠, MVC모델 기초

--- hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,13 +3,22 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 public class HelloController {
 
-    @GetMapping("sendSignal")
+    @GetMapping(value = "sendSignal")
     public String hello(Model model){
         model.addAttribute("data", "Value");
         return "page1";
     }
+
+    @GetMapping(value="helloMvc")
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "page2";
+    }
+    
 }
