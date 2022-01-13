c60ea62feb1d67ef1d88e6a71aed3ee5b30be172
BumBamBi <dlrhkddn0325@gmail.com>
2022-01-14 00:14:02 +0900

    test에서 findAll() 테스트 작성 및 AfterEach 작성

--- hello-spring/src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
+++ hello-spring/src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
@@ -2,6 +2,9 @@ package hello.hellospring.repository;
 
 import hello.hellospring.domain.Member;
 
+import java.util.List;
+
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -9,11 +12,20 @@ import org.junit.jupiter.api.Test;
 class MemoryMemberRepositoryTest {
     // 테스트할 객체 생성
     // 인터페이스이지만 객체생성가능?
-    MemberRepository repository = new MemoryMemberRepository();
+    MemoryMemberRepository repository = new MemoryMemberRepository();
+
+    
+    // Test순서에 따라 repository에 이미 값이 있어서 에러가 발생할 수 있음 -> 의존관계가 없어야함 -> repository를 초기화 해줘야함.
+    // 매번 테스트가 끝나면 이곳으로 옴 -> 초기화 해줘야함.
+    // 초기화를 위해 MemoryMemberRepository() 객체에 초기화 메서드 추가
+    @AfterEach
+    public void agterEach(){
+        repository.clearStore();
+    }
 
     // 테스트 가능
-    // 이름은 안헷갈리게 동일하게 한거임
     @Test
+    // 이름은 안헷갈리게 동일하게 한거임
     public void save(){
         // 멤버 객체 생성 후 Name 설정
         Member member = new Member();
@@ -48,10 +60,26 @@ class MemoryMemberRepositoryTest {
         member2.setName("spring2");
         repository.save(member2);
 
-        
         // repository에서 findByName()과 Name을 통해 검색
         Member result = repository.findByName("spring1").get(); // spring2이면 아랫줄에서 에러
         
         Assertions.assertEquals(member1, result); 
+    }
+
+    @Test
+    public void findAll(){
+        
+        // member1 생성 및 Name 설정 후 저장
+        Member member1 = new Member();
+        member1.setName("spring1");
+        repository.save(member1);
+        
+        // member2 생성 및 Name 설정 후 저장
+        Member member2 = new Member();
+        member2.setName("spring2");
+        repository.save(member2);
+
+        List<Member> result = repository.findAll();
+        Assertions.assertEquals(result.size(), 2);
     }
 }
